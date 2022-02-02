eb3e4d064cdb7f3ba62b89f1da0751b33b7df82f
Mitchell Mohorovich <mitchell@locallogic.co>
2022-02-01 20:00:53 -0500

    Organize as subtests, add assertion func

--- learning-go/main_test.go
+++ learning-go/main_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mitchell")
-	want := "Hello, Mitchell!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("say 'Hello, World', when the empty string is supplied.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say hello to a person", func(t *testing.T) {
+		got := Hello("Mitchell")
+		want := "Hello, Mitchell"
+		assertCorrectMessage(t, got, want)
+	})
 }
