081e8cd218037fdd0cf5edf24ebd1ead9ffc93d6
Mitchell Mohorovich <mitchell@locallogic.co>
2022-02-13 21:51:54 -0500

    Add select module.

---
+++ learning-go/select/select.go
@@ -0,0 +1,39 @@
+package select_
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(urlA, urlB string) (winner string, error error) {
+	return ConfigurableRacer(urlA, urlB, tenSecondTimeout)
+}
+
+func ConfigurableRacer(urlA, urlB string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(urlA):
+		return urlA, nil
+	case <-ping(urlB):
+		return urlB, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", urlA, urlB)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
